43a52308dc631a19e079c6f1eac5ef49238ae9cd
charm178 <charm178@gmail.com>
2015-05-13 14:07:18 -0700

    Eliminate repetition

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
+# Classes are a way to organize FUNCTIONS which are defined using DEF keyword
 class StaticPagesController < ApplicationController
   def home
   end
 
   def help
   end
+
+  def about
+  end
 end
+
